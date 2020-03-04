9137a70c5fb06e81a4d71154691603c9cf276c79
José Doñe <josedoneguillen1992@hotmail.com>
2020-03-04 16:53:13 -0400

    Creando estructura de carpeta e inicializando proyecto de cuestionario

---
+++ cuestionario/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+ En esta actividad realizarás un programa para poner en práctica los conceptos vistos sobre los tipos de datos.
+ 
+    1. Escribe un programa en el cual se solicite al usuario que introduzca:
+        a. Nombre
+        b. Apellido
+        c. Edad
+        d. Ciudad donde vive
+        e. Si es soltero o casado
+        f. Que sueldo le gustaría percibir
+
+    2. A cada pregunta, le mostrará en pantalla lo que acaba de escribir, pero con un comentario apropiado.
+    Por ejemplo:
+        • Favor dígame su nombre: Antonio
+        • Resp. Saludos Antonio, dígame su apellido: Pérez
+        • Resp. Bienvenido Antonio Pérez….
+        • Y así sucesivamente hasta completar el cuestionario.
+
+Para realizar la entrega de la actividad utiliza un procesador de texto y coloca las pantallas donde se evidencie el proceso realizado, 
+entrega de la forma como se especifica a continuación. 
+*/
+
+using System;
+
+namespace cuestionario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
