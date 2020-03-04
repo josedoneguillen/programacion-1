62e9af5c8d1040b3d62f89eb6144b70695630c49
José Doñe <josedoneguillen1992@hotmail.com>
2020-03-04 17:28:23 -0400

    Avanzando con preguntas del programa

--- cuestionario/Program.cs
+++ cuestionario/Program.cs
@@ -28,7 +28,54 @@ namespace cuestionario
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Cuestionario");
+
+            // Declaracion de variales
+            string firstName;
+            string lastName;
+            int age;
+            string city;
+            bool married = false;
+            double salary;
+
+            // Solicitar al usuario que ingrese su nombre
+            Console.WriteLine("Favor dígame su nombre:");
+
+            // Almacenar el valor introducido por el cliente el la variable firstName
+            firstName = Console.ReadLine();
+
+            // Solicitar al usuario que ingrese su apellido
+            Console.WriteLine($"Saludos {firstName}, dígame su apellido:");
+
+            // Almacenar el valor introducido por el cliente el la variable lastName
+            lastName = Console.ReadLine();
+
+            // Solicitar al usuario que ingrese su edad
+            Console.WriteLine($"Bienvenido {firstName} {lastName}, cual es su edad:");
+
+            // Almacenar el valor introducido por el cliente el la variable age
+            age = Convert.ToInt32(Console.ReadLine());
+
+            // Solicitar al usuario que ingrese su ciudad
+            Console.WriteLine($"{firstName} {lastName}, si tiene {age} años, cual es su ciudad:");
+
+            // Almacenar el valor introducido por el cliente el la variable city
+            city = Console.ReadLine();
+
+            // Solicitar al usuario que ingrese su ciudad
+            Console.WriteLine($"{firstName} {lastName}, usted tiene {age} años y vive en {city}, digame si esta casado:");
+            Console.WriteLine("1- Si" + Environment.NewLine + "2- No");
+
+            // Almacenar el valor introducido por el cliente el la variable city
+            married = (Console.ReadLine() == "1") ? true : false;
+
+            // Solicitar al usuario que ingrese su sueldo deseado
+            Console.WriteLine($"{firstName} {lastName}, con {age} años viviendo en {city}, Que sueldo le gustaría percibir:");
+
+            // Almacenar el valor introducido por el cliente el la variable salary
+            salary = Convert.ToDouble(Console.ReadLine());
+
+
         }
     }
 }
