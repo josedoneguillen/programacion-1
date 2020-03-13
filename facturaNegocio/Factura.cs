c3f1ddabc01b2f7a1f43202fcb9ede42dfb0337c
José Doñe <josedoneguillen1992@hotmail.com>
2020-03-13 11:45:15 -0400

    Proyecto factura orientada a objetos terminado

---
+++ facturaNegocio/Factura.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace facturaNegocio
+{
+    class Factura
+    {
+        /* Atributos */
+        private string noPieza;
+        private string descPieza;
+        private int cantidad;
+        private double precioUnd;
+
+        /* Constructor para inicializar atributos */
+        public Factura(string noPieza, string descPieza, int cantidad, double precioUnd) 
+        {
+            /* Asignacion de los atributos por valor en los parametros del constructor de la clase */
+            this.noPieza = noPieza;
+            this.descPieza = descPieza;
+            this.cantidad = cantidad;
+            this.precioUnd = precioUnd;
+        }
+
+        /* Metodo para leer y acceder al atributo noPieza */
+        public string NoPieza {
+            get 
+            {
+                return noPieza;
+            }
+            set 
+            {
+                noPieza = value;
+            }
+        }
+
+        /* Metodo para leer y acceder al atributo descPieza */
+        public string DescPieza
+        {
+            get
+            {
+                return descPieza;
+            }
+            set
+            {
+                descPieza = value;
+            }
+        }
+
+        /* Metodo para leer y acceder al atributo cantidad */
+        public int Cantidad
+        {
+            get
+            {
+                return cantidad;
+            }
+            set
+            {
+                /* Solo modificar el atributo cantidad si el valor recibido es positivo */
+                if (value > 0) 
+                {
+                    cantidad = value;
+                }
+                
+            }
+        }
+
+        /* Metodo para leer y acceder al atributo precioUnd */
+        public double PrecioUnd
+        {
+            get
+            {
+                return precioUnd;
+            }
+            set
+            {
+                /* Solo modificar el atributo precioUnd si el valor recibido es positivo */
+                if (value > 0)
+                {
+                    precioUnd = value;
+                }
+
+            }
+        }
+
+        /* Metodo para obtener el valor de la factura en tipo decimal */
+        public decimal GetMontoFactura()
+        {
+            return (decimal)(this.cantidad * this.precioUnd);
+        }
+
+    }
+}
---
+++ facturaNegocio/Program.cs
@@ -0,0 +1,46 @@
+﻿/*
+Esta actividad consiste en la creación de una clase. La clase se debe llamar Factura, esta factura pudiera ser usada por un negocio para presentar un artículo vendido. Realiza lo siguiente:
+
+a) Debe incluir cuatro piezas de información como variables de instancia o propiedades automáticas:
+1. Un número de pieza - tipo string.
+2. Descripción de la pieza – Tipo string.
+3. Cantidad del artículo que se está comprando - Tipo int
+4. Precio por artículo – Tipo decimal.
+
+b) La clase debe tener un constructor que inicialice los cuatro valores. Crear un método para acceder y leer cada variable.
+c) Validar que, si se pasa un valor negativo para cantidad o precio, el valor de la variable de instancia no debe modificarse.
+d) Crear un método llamado GetMontoFactura que calcule el importe de la factura y lo devuelva como un valor decimal.
+e) Escribe un programa donde muestre el funcionamiento de la clase.
+
+Para realizar la entrega de la actividad utiliza un procesador de texto y coloca las pantallas donde se evidencie el proceso realizado, entrega de la forma como se especifica a continuación.
+*/
+using System;
+
+namespace facturaNegocio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido al programa para ver facturas");
+
+            // Creamos una instancia de la clase factura es decir un objeto
+            Factura fact1 = new Factura("123456", "Una pieza de prueba", 10, 10.95);
+
+            // Obtener descripcion de la factura
+            Console.WriteLine(fact1.DescPieza);
+
+            // Modificar cantidad de la factura por un numero negativo
+            fact1.Cantidad = -10;
+
+            // Obtener cantidad de la factura
+            Console.WriteLine(fact1.Cantidad);
+
+            // Obtener el total de la factura llamando el metodo GetMontoFactura
+            Console.WriteLine(fact1.GetMontoFactura());
+
+            // Esperar letra para cerrar la consola
+            Console.ReadKey();
+        }
+    }
+}
