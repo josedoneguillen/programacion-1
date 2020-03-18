fa0430ed0ccc4b7bd5149076dd4b7236d4b70af6
José Doñe <josedoneguillen1992@hotmail.com>
2020-03-17 21:34:17 -0400

    Programa para esctibir tu nombre en consola terminado

---
+++ Nombre/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Nombre
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaracion de variables
+            string Name;
+
+            // Solicitar el nombre del usuario
+            Console.WriteLine("Ingrese su nombre");
+
+            // Almacenar la linea en la variable Name
+            Name = Console.ReadLine();
+
+            // Imprimir en resultado en pantalla
+            Console.WriteLine("Hola, " + Name);
+
+            // Esperar para cerrar
+            Console.ReadKey();
+        }
+    }
+}
