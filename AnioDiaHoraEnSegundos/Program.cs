217d1574b73c9cec707991a6c7f4225439a9e390
José Doñe <josedoneguillen1992@hotmail.com>
2020-03-17 21:07:29 -0400

    Proyecto calcular los segundos en una hora en un dia y un anio terminado

---
+++ AnioDiaHoraEnSegundos/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AnioDiaHoraEnSegundos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Declaracion de variables */
+            int Min = 60;
+            int Hour = Min * Min;
+            int Day = Hour * 24;
+            int Year = Day * 365;
+
+            /* Imprimir resultados */
+            Console.WriteLine($"Segundos en una hora: {Hour}");
+            Console.WriteLine($"Segundos en un dia: {Day}");
+            Console.WriteLine($"Segundos en un año: {Year}");
+
+            //Esperar para cerrar
+            Console.ReadKey();
+        }
+    }
+}
