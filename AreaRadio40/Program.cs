e07b5637ca925312038389bab063e11146e43b1c
José Doñe <josedoneguillen1992@hotmail.com>
2020-03-17 21:51:47 -0400

    Programa para calcular el area de un circulo de radio 40 terminado

---
+++ AreaRadio40/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AreaRadio40
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaracion de variables
+            double Area, Radio = 40;
+
+            //Realizar calculo
+            Area = Radio * Radio * Math.PI;
+
+            // Imprimir resultado
+            Console.WriteLine($"El area del circulo es de: {Area}");
+
+            // Esperar para cerrar
+            Console.ReadKey();
+        }
+    }
+}
