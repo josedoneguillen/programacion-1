eb7a7eebe26395b0aef275e3157d8f4f1520f153
José Doñe <josedoneguillen1992@hotmail.com>
2020-03-17 21:18:10 -0400

    Programa para mostrar la raiz cuadrada y el logaritmo del numero nueve terminado

---
+++ CalcularRaizLog/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CalcularRaizLog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaracion de variables
+            double Log9 = Math.Log(9);
+            double Sqrt9 = Math.Sqrt(9);
+
+            // Imprimir en resultado en pantalla
+            Console.WriteLine("El Logaritmo de 9 es: " + Log9);
+            Console.WriteLine("La Raiz Cuadrada de 9 es: " + Sqrt9);
+
+            // Esperar para cerrar
+            Console.ReadKey();
+        }
+    }
+}
