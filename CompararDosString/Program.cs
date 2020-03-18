37805a1b60230674b4ad3e5bc422bcc9d8924562
José Doñe <josedoneguillen1992@hotmail.com>
2020-03-17 21:42:44 -0400

    Programa para comparar dos string terminado

---
+++ CompararDosString/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace CompararDosString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaracion de variables
+            string value1, value2;
+
+            // Solicitar valores al usuario
+            Console.WriteLine("Introduzca primer valor");
+            value1 = Console.ReadLine();
+
+            Console.WriteLine("Introduzca segundo valor");
+            value2 = Console.ReadLine();
+
+            // Declaracion if para comparar los dos valores
+            if (value1 == value2)
+            {
+                Console.WriteLine(value1 + " y " + value1 + " son iguales");
+            }
+            else
+            {
+                Console.WriteLine(value1 + " y " + value1 + " son diferentes");
+            }
+
+            // Esperar para cerrar
+            Console.ReadKey();
+        }
+    }
+}
